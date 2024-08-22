5debef08a5cb38ca6a52e6476075e87debaadd1a
Joel Burget <joelburget@gmail.com>
2024-08-22 10:22:26 -0700

    Remove migrations with duplicate content.

--- prisma/migrations/20240821204005_make_url_slug_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[urlSlug]` on the table `Poll` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Poll_urlSlug_key" ON "Poll"("urlSlug");
