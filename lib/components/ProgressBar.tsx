f32398a613b6bb4c57d293422894b544bd87ec9a
joelburget <joelburget@users.noreply.github.com>
2024-08-23 00:09:43 +0000

    Prettified Code!

--- lib/components/ProgressBar.tsx
+++ lib/components/ProgressBar.tsx
@@ -30,14 +30,16 @@ function ProgressBarItem({
   return (
     <div className="flex flex-col items-center gap-2 mb-4 w-[80px]">
       <div
-        className={`rounded-full h-6 w-6 flex justify-center items-center text-mono text-xs text-white font-medium ${isActive ? "bg-[#185849]" : "bg-[#E0E0E0]"
-          }`}
+        className={`rounded-full h-6 w-6 flex justify-center items-center text-mono text-xs text-white font-medium ${
+          isActive ? "bg-[#185849]" : "bg-[#E0E0E0]"
+        }`}
       >
         {stage}
       </div>
       <h2
-        className={`text-sm font-medium ${isActive ? "text-[#185849]" : "text-[#E0E0E0]"
-          }`}
+        className={`text-sm font-medium ${
+          isActive ? "text-[#185849]" : "text-[#E0E0E0]"
+        }`}
       >
         {label}
       </h2>
