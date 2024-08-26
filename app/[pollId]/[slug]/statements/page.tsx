adbd925758f5c9167352d6053df4238af9443493
Joel Burget <joelburget@gmail.com>
2024-08-26 10:26:39 -0700

    Note if there are already statements. Maybe we want to remove this link instead?

--- app/[pollId]/[slug]/statements/page.tsx
+++ app/[pollId]/[slug]/statements/page.tsx
@@ -5,16 +5,24 @@ import ProgressBar from "@/lib/components/ProgressBar";
 import PollStatements from "@/lib/components/edit-poll/Statements";
 
 export default async function Page({ params }: { params: { pollId: string } }) {
-  const poll = await prisma.poll.findUnique({
-    where: { uid: params.pollId },
-  });
+  const [poll, existingStatementCount] = await Promise.all([
+    prisma.poll.findUnique({
+      where: { uid: params.pollId },
+    }),
+    prisma.statement.count({
+      where: { pollId: params.pollId },
+    }),
+  ]);
   if (!poll || poll.deleted) return notFound();
 
   return (
     <div className="flex flex-col">
       <PageTitle title="New Poll" />
       <ProgressBar step="statements" poll={poll} />
-      <PollStatements poll={poll} />
+      <PollStatements
+        poll={poll}
+        existingStatementCount={existingStatementCount}
+      />
     </div>
   );
 }
--- lib/components/edit-poll/Statements.tsx
+++ lib/components/edit-poll/Statements.tsx
@@ -8,15 +8,26 @@ import Button from "@/lib/components/Button";
 import { publishPoll } from "@/lib/actions";
 import { Poll } from "@prisma/client";
 
-export default function PollStatements({ poll }: { poll: Poll }) {
+export default function PollStatements({
+  poll,
+  existingStatementCount,
+}: {
+  poll: Poll;
+  existingStatementCount: number;
+}) {
   const [statements, setStatements] = useState<string>("");
   const router = useRouter();
+
+  const description =
+    existingStatementCount === 0
+      ? "Statements are prompts for participants to respond to (by selecting ‘agree’, ‘disagree’, or ‘pass’). Enter at least 5 to start."
+      : `Your poll already has ${existingStatementCount} statements. Add more?`;
 
   return (
     <>
       <Textarea
         label="Statements"
-        description="Statements are prompts for participants to respond to (by selecting ‘agree’, ‘disagree’, or ‘pass’). Enter at least 5 to start."
+        description={description}
         value={statements}
         setValue={setStatements}
       />
