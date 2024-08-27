3e24def7d1b28510da78f3134f347c2ee53a66a3
Chris John <cxjohn@pm.me>
2024-08-27 12:41:35 -0400

    hiding poll controls behind a creator check

--- lib/components/PollCard.tsx
+++ lib/components/PollCard.tsx
@@ -7,6 +7,7 @@ import ConstitutionIcon from "./icons/ConstitutionIcon";
 import IconCounter from "./IconCounter";
 import ParticipantIcon from "./icons/ParticipantIcon";
 import PollCardControls from "./PollCardControls";
+import { isCreator } from "../isCreator";
 
 export default async function PollCard({ poll }: { poll: Poll }) {
   const statements = await prisma.statement.findMany({
@@ -15,6 +16,8 @@ export default async function PollCard({ poll }: { poll: Poll }) {
   const votes = await prisma.vote.findMany({
     where: { statementId: { in: statements.map(({ uid }) => uid) } },
   });
+
+  const isUserCreator = await isCreator(poll.creatorId);
 
   return (
     <div className="bg-[#FAFAFA] p-6 rounded w-[284px]">
@@ -32,7 +35,7 @@ export default async function PollCard({ poll }: { poll: Poll }) {
           <IconCounter count={votes.length} icon={<ParticipantIcon />} />
           <IconCounter count={statements.length} icon={<StatementIcon />} />
         </div>
-        <PollCardControls poll={poll} />
+        {isUserCreator && <PollCardControls poll={poll} />}
       </div>
       <div className="flex justify-center">
         <Button
