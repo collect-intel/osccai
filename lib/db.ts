dc28c55817afe949cdf75d42caba06a3b7c6be83
evanhadfield <147835824+evanhadfield@users.noreply.github.com>
2024-09-30 10:14:03 -0600

    add update, delete to vote count middleware

--- lib/db.ts
+++ lib/db.ts
@@ -8,24 +8,78 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 // middleware to update statement vote counts on vote creation
 prisma.$use(async (params, next) => {
-  if (params.model === "Vote" && params.action === "create") {
+  if (params.model === "Vote") {
     const result = await next(params);
 
-    // Update the corresponding statement's vote counts
-    await prisma.statement.update({
-      where: { uid: result.statementId },
-      data: {
-        agreeCount: {
-          increment: result.voteValue === VoteValue.AGREE ? 1 : 0,
+    if (params.action === "create") {
+      // Update the corresponding statement's vote counts on vote creation
+      await prisma.statement.update({
+        where: { uid: result.statementId },
+        data: {
+          agreeCount: {
+            increment: result.voteValue === VoteValue.AGREE ? 1 : 0,
+          },
+          disagreeCount: {
+            increment: result.voteValue === VoteValue.DISAGREE ? 1 : 0,
+          },
+          passCount: {
+            increment: result.voteValue === VoteValue.PASS ? 1 : 0,
+          },
         },
-        disagreeCount: {
-          increment: result.voteValue === VoteValue.DISAGREE ? 1 : 0,
+      });
+    } else if (params.action === "update") {
+      // Update the corresponding statement's vote counts on vote update
+      const { voteValue: newVoteValue, statementId } = result;
+      const { voteValue: oldVoteValue } = params.args.data;
+
+      await prisma.statement.update({
+        where: { uid: statementId },
+        data: {
+          agreeCount: {
+            increment:
+              newVoteValue === VoteValue.AGREE && oldVoteValue !== VoteValue.AGREE
+                ? 1
+                : newVoteValue !== VoteValue.AGREE && oldVoteValue === VoteValue.AGREE
+                ? -1
+                : 0,
+          },
+          disagreeCount: {
+            increment:
+              newVoteValue === VoteValue.DISAGREE && oldVoteValue !== VoteValue.DISAGREE
+                ? 1
+                : newVoteValue !== VoteValue.DISAGREE && oldVoteValue === VoteValue.DISAGREE
+                ? -1
+                : 0,
+          },
+          passCount: {
+            increment:
+              newVoteValue === VoteValue.PASS && oldVoteValue !== VoteValue.PASS
+                ? 1
+                : newVoteValue !== VoteValue.PASS && oldVoteValue === VoteValue.PASS
+                ? -1
+                : 0,
+          },
         },
-        passCount: {
-          increment: result.voteValue === VoteValue.PASS ? 1 : 0,
+      });
+    } else if (params.action === "delete") {
+      // Update the corresponding statement's vote counts on vote deletion
+      const { voteValue, statementId } = params.args.where;
+
+      await prisma.statement.update({
+        where: { uid: statementId },
+        data: {
+          agreeCount: {
+            decrement: voteValue === VoteValue.AGREE ? 1 : 0,
+          },
+          disagreeCount: {
+            decrement: voteValue === VoteValue.DISAGREE ? 1 : 0,
+          },
+          passCount: {
+            decrement: voteValue === VoteValue.PASS ? 1 : 0,
+          },
         },
-      },
-    });
+      });
+    }
 
     return result;
   }
